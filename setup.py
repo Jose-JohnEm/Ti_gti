33fb6e91fe14fa1d2d57c5b42410657c411a55fa
José Jonathan Emmanuel <jonthan-emmanuel.jose@epitech.eu>
2020-05-12 23:45:05 +0200

    Installer is almost finish I jsut need to add PATH variable

--- setup.py
+++ setup.py
@@ -13,25 +13,59 @@ def os_is_not_compatible(osys):
 def exit_error(err):
     errs = ["", "Sorry gti only works on Windows, Linux, and Mac os",
             "Install failed because some files are missing. Please redownload the module",
+            "Unknown shell. Please set cmd.exe as your default shell",
+            "Pip installation failed... Make sure you have Internet connection",
+            "Pyinstaller installation Failed... Make sure you have Internet connection",
+            "Gti compilation failed... Please redownload the module",
             ""]
     for i in range(1, len(errs)):
         if i == err:
             print(errs[i])
     exit(err)
 
+def configure_pip_on_windows():
+    sh = os.environ['COMSPEC']
+    if sh.find("cmd.exe") == 0:
+        exit_error(3)
+    ret = os.system("cd setup\ > null")
+    os.system("del /f null")
+    if ret != 0:
+        exit_error(2)
+    ret = os.system("cd .\setup\ && python get-pip.py > null")
+    os.system("del /f .\setup\\null")
+    if ret != 0:
+        exit_error(4)
+    print("Pip has been correctly installed ! [1/4]")
+
+def configure_pip_on_linux_and_mac():
+    pass
+
 def install_pip(osys):
     if osys == "Windows":
-        sh = os.environ['SHELL']
-        print(sh)
-        #ret = os.system("cd setupsss/ > null")
-        #os.system("del /f null")
-        #if ret != 0:
-        #    exit_error(2)
-        #ret = os.system("python get-pip.py")
-        return
+        configure_pip_on_windows()
     else:
-        return
+        configure_pip_on_linux_and_mac()
 
+def install_pyinstaller(osys):
+    ret = os.system("pip install pyinstaller --quiet")
+    if ret != 0:
+        exit_error(5)
+    if osys == "Windows":
+        os.system("del /f .\\null")
+    else:
+        os.system("rm -f null")
+    print("Pyinstaller has been correctly installed ! [2/4]")
+
+def install_gti(osys):
+    ret = os.system("pyinstaller --onefile gti.py --name gti > nl")
+    if ret != 0:
+        exit_error(6)
+    if osys == "Windows":
+        pass
+    else:
+        os.system("rm -rf build; mkdir ~/jose-git; mv dist/gti.exe ~/jose-git/gti; rmdir dist")
+    print("Gti has been correctly installed ! [3/4]")
+    
 
 def main():
     osys = platform.system()
@@ -39,6 +73,8 @@ def main():
     if os_is_not_compatible(osys):
         exit_error(1)
     install_pip(osys)
+    install_pyinstaller(osys)
+    install_gti(osys)
     print("Sucess !")
 
 
